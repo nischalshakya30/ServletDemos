edea7a20d02ed2a637e470c3519c7d05c763ad9a
nischalshakya30 <nischalshakya@iimscollege.edu.np>
2020-09-25 08:18:46 +0545

    Created a sample demo of login

--- src/java/org/iims/servletdemos/Login.java
+++ src/java/org/iims/servletdemos/Login.java
@@ -7,7 +7,9 @@ package org.iims.servletdemos;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +18,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author msii
  */
-public class ServletDemo extends HttpServlet {
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class Login extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -30,19 +33,8 @@ public class ServletDemo extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletDemo</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletDemo at " + request.getContextPath() + "</h1>");
-            out.println("<h1>Servlet ServletDemo at " + "this is first servlet class" + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.html");
+        requestDispatcher.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -71,7 +63,21 @@ public class ServletDemo extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
+        PrintWriter out = response.getWriter();
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username.equals("admin") && password.equals("admin")) {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("welcome");
+            requestDispatcher.forward(request, response);
+        } else {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("login.html");
+            out.println("Invalid Credentials");
+            requestDispatcher.include(request, response);
+        }
+
     }
 
     /**
